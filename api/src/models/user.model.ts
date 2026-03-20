e32796d70d12ae259285cf55c3d701c3ca2e1901
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2026-03-20 10:09:56 -0700

    feat: add saveUser updateUser and deleteUser functions

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,5 +1,5 @@
 import { pool } from '../db/connection';
-import { RowDataPacket } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { User } from '../types/Users.interface';
 
 export const findUsers = async (): Promise<User[]> => {
@@ -8,10 +8,34 @@ export const findUsers = async (): Promise<User[]> => {
 };
 
 export const findUser = async (id: number): Promise<User> => {
-    const [rows] = await pool.query<RowDataPacket[]>('SELECT * FROM users where id = ?', [id]);
+    const [rows] = await pool.query<RowDataPacket[]>('SELECT * FROM users WHERE id = ?', [id]);
     if (rows.length === 0) {
         return {} as User;
     }
 
     return rows[0] as User;
 }
+
+export const saveUser = async (userData: User): Promise<boolean> => {
+    const [result] = await pool.query<ResultSetHeader>(
+        'INSERT INTO users (name, email, password_hash, role, active) VALUES (?, ?, ?, ?, ?)',
+        [userData.name, userData.email, userData.password_hash, userData.role, true]
+    );
+    return result.affectedRows > 0
+}
+
+export const updateUser = async (userData: User): Promise<boolean> => {
+    const [result] = await pool.query<ResultSetHeader>(
+        'UPDATE users SET name = ?, email = ? WHERE id = ?',
+        [userData.name, userData.email, userData.id]
+    );
+    return result.affectedRows > 0
+}
+
+export const deleteUser = async (userId: number): Promise<boolean> => {
+    const [result] = await pool.query<ResultSetHeader>(
+        'DELETE FROM users WHERE id = ?',
+        [userId]
+    );
+    return result.affectedRows > 0
+} 
--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,4 +1,5 @@
 import * as userModel from '../models/user.model';
+import { User } from '../types/Users.interface';
 
 export const findUsers = async () => {
     return await userModel.findUsers()
@@ -6,4 +7,16 @@ export const findUsers = async () => {
 
 export const findUser = async (userId: number) => {
     return await userModel.findUser(userId)
+}
+
+export const saveUser = async (userData: User) => {
+    return await userModel.saveUser(userData)
+}
+
+export const updateUser = async (userData: User) => {
+    return await userModel.updateUser(userData)
+}
+
+export const deleteUser = async (userId: number) => {
+    return await userModel.deleteUser(userId)
 }
