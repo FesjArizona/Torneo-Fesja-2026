3bd72fe0ebe2c8f89befeaaac1246519ccce18b9
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2026-03-25 21:49:09 -0700

    feat: add team inscription, update, and deletion (#8)
    * feat: add getUsers and getuser functions
    
    * feat: add saveUser updateUser and deleteUser functions
    
    * prueba
    
    * Delete api/src/config/environment.ts
    
    * prueba
    
    * feat: implements crud to tournaments
    
    * Add team crud
    
    * feat: add team inscription, update, and deletion (#7)

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { tournamentsRouter } from './tournaments.routes';
 import { userRoutes } from './users.routes';
 import { teamRoutes } from './team.routes';
+import { inscriptionRoutes } from './inscription.routes';
 
 export const router = Router();
 
 router.use('/tournament', tournamentsRouter);
 router.use('/user', userRoutes);
 router.use('/team', teamRoutes);
+router.use('/', inscriptionRoutes);
