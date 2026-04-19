cb2e0af05496f654131fb9117472bc80d929e985
Jared Márquez <jared.marquezb@gmail.com>
2026-04-19 02:00:18 -0600

    Ts in login for auth

--- src/app/admin/auth/auth/auth.component.ts
+++ src/app/admin/auth/auth/auth.component.ts
@@ -1,11 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent { }
+export class AuthComponent implements OnInit {
+  authForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.authForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+    onSubmited() {
+      console.log('Email sent', this.authForm);
+  }
+
+}
