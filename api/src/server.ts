aa10139072e5a611155e22ef5dff9a256e6ccd1d
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2026-07-28 21:02:31 -0700

    Convert port to a number for server configuration

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import { env } from './config/environment';
 
-const port = process.env.PORT || env.PORT;
+const port = Number(process.env.PORT || env.PORT);
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`API corriendo en el puerto ${port} (0.0.0.0)`);
